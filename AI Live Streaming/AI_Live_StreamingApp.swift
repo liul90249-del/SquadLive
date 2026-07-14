a5a46564c13200b13a7feb208591237df3363d95
liuzhigang <1655896527@qq.com>
2026-07-14 11:37:09 +0800

    Initial Commit

---
+++ AI Live Streaming/AI_Live_StreamingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_Live_StreamingApp.swift
+//  AI Live Streaming
+//
+//  Created by liu on 2026/7/14.
+//
+
+import SwiftUI
+
+@main
+struct AI_Live_StreamingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
